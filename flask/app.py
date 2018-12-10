924e8a5d8eefb57deb1aaf80254fe17c0c4a0435
halflkaka <cs3736@columbia.edu>
2018-12-10 18:54:30 -0500

    Update layout, react-table

--- flask/app.py
+++ flask/app.py
@@ -16,16 +16,17 @@ def tokenize(s): return re_tok.sub(r' \1 ', s).split()
 re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
 label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
-count = {'severe_toxic':10, 'obscene':10, 'threat':10, 'insult':10, 'identity_hate':10}
+count = {'severe_toxic':0, 'obscene':0, 'threat':0, 'insult':0, 'identity_hate':0}
+users = {'severe_toxic':[], 'obscene':[], 'threat':[], 'insult':[], 'identity_hate':[]}
 
 with open("vesc", "rb") as v:
-	vec = pickle.load(v)
+    vec = pickle.load(v)
 
 with open("model_param1", "rb") as file1:
-	model1 = pickle.load(file1)
+    model1 = pickle.load(file1)
 
 with open("model_param2", "rb") as file2:
-	model2 = pickle.load(file2)
+    model2 = pickle.load(file2)
 
 class listener(StreamListener):
 
@@ -37,7 +38,7 @@ class listener(StreamListener):
     def on_data(self, data):
         if (time.time() - self.start_time) < self.limit:
             all_data = json.loads(data)
-            print(all_data)
+            # print(all_data)
             
             try:
                 tweet = all_data["text"]
@@ -47,13 +48,14 @@ class listener(StreamListener):
                 preds = np.zeros(len(label_cols))
                 for i in range(len(label_cols)):
                     preds[i] = model1[i].predict_proba(vectorized_data.multiply(model2[i]))[:,1]
-                print (preds)
                 if (preds[0] > 0.5):
                     print("it is toxic")
                     for i in range(1, len(preds)):
                         if (preds[i] > 0.3):
                             count[label_cols[i]] += 1
-                print(tweet)
+                            user = {'name': all_data["user"]["name"], "tweet": all_data["text"]}
+                            users[label_cols[i]].append(user)
+                # print(tweet)
             except KeyError as e:
                 pass
             return True
@@ -64,20 +66,34 @@ class listener(StreamListener):
         print (status)
 
 def start():
-	#consumer key, consumer secret, access token, access secret.
-	ckey=CONF["APP_KEY"]
-	csecret=CONF["APP_SECRET"]
-	atoken=CONF["OAUTH_TOKEN"]
-	asecret=CONF["OAUTH_TOKEN_SECRET"]
+    #consumer key, consumer secret, access token, access secret.
+    ckey=CONF["APP_KEY"]
+    csecret=CONF["APP_SECRET"]
+    atoken=CONF["OAUTH_TOKEN"]
+    asecret=CONF["OAUTH_TOKEN_SECRET"]
 
-	auth = OAuthHandler(ckey, csecret)
-	auth.set_access_token(atoken, asecret)
+    auth = OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
 
-	twitterStream = Stream(auth, listener(time_limit=5))
-	twitterStream.sample()
+    twitterStream = Stream(auth, listener(time_limit=5))
+    twitterStream.sample(languages=['en'])
 
+def getdata():
+    res = []
+    data = []
+    bar = []
+    blacklist = []
+    for key in count:
+        data.append({'key': key, 'value': count[key]})
+        bar.append({'x': key, 'y': count[key]})
+    res.append(data)
+    res.append(bar)
+    blacklist.append(users)
+    res.append(blacklist)
+    response = jsonify(res)
+    return response
 # if __name__ == "__main__":
-# 	start()
+#   start()
 
 start()
 
@@ -88,34 +104,15 @@ def index():
     return jsonify({'msg':'Hello'})
 
 @app.route("/chart", methods=["GET"])
-def getdata():
-    res = []
-    data = []
-    bar = []
-    for key in count:
-        data.append({'key': key, 'value': count[key]})
-        bar.append({'x': key, 'y': count[key]})
-    res.append(data)
-    res.append(bar)
-    response = jsonify(res)
-    print("resp: "+response)
+def run():
+    response = getdata()
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.route("/refresh", methods=["GET"])
 def refresh():
     start()
-    res = []
-    data = []
-    bar = []
-    for key in count:
-        data.append({'key': key, 'value': count[key]})
-        bar.append({'x': key, 'y': count[key]})
-    # response = jsonify(data)
-    res.append(data)
-    res.append(bar)
-    response = jsonify(res)
-    print(response)
+    response = getdata()
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {PieChart, BarChart, Legend} from 'react-easy-chart';
-import { Button, Grid, Row} from 'react-bootstrap';
+import { Button, Grid, Row, Col} from 'react-bootstrap';
+import ReactTable from 'react-table'
+import '../node_modules/react-table/react-table.css'
+import './App.css'
+// import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+// import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -17,7 +22,8 @@ export default class App extends React.Component {
           {x: 'A', y:100},
           { x: 'B', y: 200 },
           { x: 'C', y: 50 }
-        ]
+        ],
+        blacklist:[{'obscene':[]}]
       };
       this.refresh = this.refresh.bind(this);
   }
@@ -33,7 +39,8 @@ export default class App extends React.Component {
             this.setState({
               isLoaded: true,
               items: result[0],
-              bar: result[1]
+              bar: result[1],
+              blacklist: result[2]
             });
           },
           (error) => {
@@ -48,53 +55,100 @@ export default class App extends React.Component {
       this.refresh();
   }
   render () {
-    const { error, isLoaded, items, bar } = this.state;
+    const { error, isLoaded, items, bar, blacklist } = this.state;
+    console.log(blacklist)
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name',
+      width: 500, // String-based value accessors!
+      height: 500
+    }, {
+      Header: 'Tweet',
+      accessor:'tweet',
+      width: 500,
+      height: 500
+    }];
+    const customStyle = {
+      '.legend': {
+        margin: 10
+      }
+    };
     if (error) {
         return <div>Error: {error.message}</div>;
     } else {
       console.log("draw...");
       console.log(items);
       return (
+        <div>
       <Grid>
-         <Row className="text-center">
+         <Row>
+         <Col md={3}/>
+         <Col md={6}>
             <h1>Toxic Comment Classification</h1>
+          </Col>
+          <Col md={4}/>
          </Row>
-         <Row className="text-center">
-             <div style={{display: 'inline-block'}}>
-                   <BarChart axes colorBars grid
-                              height={350}
-                              width={650}
-                              data={bar}
-                              clickHandler={(d) => this.setState({barDataDisplay: `The value on the ${d.x} is ${d.y}`})}/>
-            </div>
+         <Row>
+          <Col md={5}/>
+          <Col md={6}>
+             <Button bsStyle="primary" onClick={this.refresh}>{isLoaded? 'Refresh':'Loading'}</Button>
+          </Col>
+          <Col md={4}/>
           </Row>
+         <Row>
+         <Col md={1}/>
+         <Col md={8}>
+              <BarChart axes colorBars grid
+                  height={550}
+                  width={850}
+                  data={bar}
+                  clickHandler={(d) => this.setState({barDataDisplay: `The value on the ${d.x} is ${d.y}`, selected:d.x})}/>
+        </Col>
+        <Col md={3}/>
+        </Row>
           <Row>
-            <div style={{display: 'inline-block', verticalAlign: 'top', paddingLeft: '20px'}}>
+          <Col md={4} />
+          <Col md={5}>
                 {this.state.barDataDisplay ? this.state.barDataDisplay : 'Click on a bar to show the value'}
-            </div>
+            </Col>
+          <Col md={5}/>
          </Row>
-         <Row className="text-center">
-             <div style={{display: 'inline-block'}}>
-              <PieChart labels data={items} clickHandler={
+         <Row/>
+          <div/>
+         <Row>
+          <Col md={3}/>
+          <Col md={6}>
+              <PieChart data={items} clickHandler={
                   (d) => this.setState({
-                    dataDisplay: `The value of ${d.data.key} is ${d.value}`
+                    dataDisplay: `The value of ${d.data.key} is ${d.value}`,
+                    selected: d.data.key
                   })
               }/>
-              <Legend data={items} dataId={'key'} horizontal />
-              </div>
+          </Col>
+          <Col md={4}/> 
           </Row>
           <Row>
-            <div style={{display: 'inline-block', verticalAlign: 'top', paddingLeft: '20px'}}>
+            <Col md={2}/>
+            <Col md={7}>
+              <Legend styles={customStyle} data={items} dataId={'key'} horizontal />
+            </Col>
+            <Col md={5}/>
+          </Row>
+          <Row>
+            <Col md={4} />
+            <Col md={4}>
               {this.state.dataDisplay ? this.state.dataDisplay : 'Click on a bar to show the value'}
-            </div>
+              </Col>
+            <Col md={5}/>
          </Row>
 
-         <Row className="text-center">
-         <div style={{display: 'inline-block', verticalAlign: 'top', paddingLeft: '20px'}}>
-            <Button bsStyle="primary" onClick={this.refresh}>{isLoaded? 'Refresh':'Loading'}</Button>
-          </div>
-         </Row>
+         <Row>
+            {this.state.selected? 
+              <ReactTable defaultPageSize={10} minRows={10} showPageSizeOptions={false} data={blacklist['0'][this.state.selected]} columns={columns}/>
+              : ''}
+          </Row>
       </Grid>
+      </div>
       );
     }
   }
